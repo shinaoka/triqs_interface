4fcf9a14092cb081d5baa513872f07d63a3652bc
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2018-08-03 09:49:22 +0900

    Fixed Travis CI for mac

--- travis/build_prereq.sh
+++ travis/build_prereq.sh
@@ -11,19 +11,16 @@ if [[ "$TRAVIS_OS_NAME" != "osx" ]]; then
     sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 40 --slave /usr/bin/g++ g++ /usr/bin/g++-4.8
     sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.9 60 --slave /usr/bin/clang++ clang++ /usr/bin/clang++-3.9
     sudo apt-get install -y cmake git libgfortran3 gfortran openmpi-bin openmpi-common openmpi-doc libopenmpi-dev libblas-dev liblapack-dev libfftw3-dev libgmp-dev hdf5-tools libhdf5-serial-dev python-h5py python-dev python-numpy python-scipy python-jinja2 python-virtualenv python-matplotlib python-tornado python-zmq python-mpi4py python-mako clang-format-3.9 libclang-3.9-dev python-clang-3.9 python-sphinx libjs-mathjax valgrind
-  - export OMPI_CC=${CC}
-  - export OMPI_CXX=${CXX}
 else
     brew cask uninstall oclint # TravisCI bug, see https://github.com/travis-ci/travis-ci/issues/8826#issuecomment-350103392
-    brew upgrade pyenv
-    pyenv install 2.7.15 # Use Python 2.7.15
-    pyenv global 2.7.15
+    #brew upgrade pyenv
+    #pyenv install 2.7.15 # Use Python 2.7.15
+    #pyenv global 2.7.15
     brew install llvm
     brew link --force llvm
-    echo 'export PATH="/usr/local/opt/llvm/bin:$PATH"' >> ~/.bashrc
-    echo 'export CC=clang' >> ~/.bashrc
-    echo 'export CXX=clang++' >> ~/.bashrc
-    source ~/.bashrc
+    export PATH="/usr/local/opt/llvm/bin:$PATH"
+    export CC=clang
+    export CXX=clang++
     brew install cmake
     brew install --with-mpi --with-python --without-single boost
     brew install hdf5 gsl gmp fftw open-mpi zmq
